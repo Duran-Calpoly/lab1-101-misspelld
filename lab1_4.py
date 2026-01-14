ba198816e5b80f147eb56faa5da5ae4e5d862684
misspelld <130675151+misspelld@users.noreply.github.com>
2026-01-14 23:30:23 +0000

    modified:   __pycache__/lab1_4.cpython-312.pyc modified:   __pycache__/lab1_5.cpython-312.pyc modified:   lab1_4.py modified:   lab1_5.py

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,9 @@
-pass
+def calculate_average(num1, num2, num3):
+    return (int(num1 + num2 + num3)) / 3.0
+
+def add_tax(bill_total):
+    return bill_total * 1.10
+
+def greet_user(name):
+    return "Hello " + name
+
